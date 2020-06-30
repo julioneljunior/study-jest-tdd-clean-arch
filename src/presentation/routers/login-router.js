57b263fd4c16f2771252a0ef79a4c763913b9e03
Elcio Junior <julioneljunior@gmail.com>
2020-06-29 21:58:18 -0300

    refactor(all files): update error files path

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -1,6 +1,5 @@
 const HttpResponse = require('../helpers/http-response');
-const MissingParamError = require('../helpers/missing-param-error');
-const InvalidParamError = require('../helpers/invalid-param-error');
+const { InvalidParamError, MissingParamError } = require('../errors');
 
 module.exports = class LoginRouter {
   constructor (authUseCase, emailValidator) {

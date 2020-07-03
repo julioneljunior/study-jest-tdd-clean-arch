ba1a6c635ed0341ada12f8c28c953ce788680267
Elcio Junior <julioneljunior@gmail.com>
2020-07-02 23:00:52 -0300

    feat(src/infra/repositories): add load-user-by-email-repository (#15)

---
+++ src/infra/repositories/load-user-by-email-repository.js
@@ -0,0 +1,22 @@
+const MissingParamError = require('../../utils/errors/missing-param-error');
+
+module.exports = class LoadUserByEmailRepository {
+  constructor (userModel) {
+    this.userModel = userModel;
+  }
+
+  async load (email) {
+    if (!email) {
+      return new MissingParamError('email');
+    }
+    const user = await this.userModel.findOne({
+      email
+    }, {
+      projection: {
+        password: 1,
+        email: 1
+      }
+    });
+    return user;
+  }
+};

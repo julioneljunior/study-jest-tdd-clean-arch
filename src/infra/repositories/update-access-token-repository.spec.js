901e8c5c4b0fd1af0563423557f9e8998636f9c6
Elcio Junior <julioneljunior@gmail.com>
2020-07-13 22:54:17 -0300

    feat(src/infra/repositories): add update-access-token-repository

---
+++ src/infra/repositories/update-access-token-repository.spec.js
@@ -0,0 +1,56 @@
+const MissingParamError = require('../../utils/errors/missing-param-error');
+const UpdateAccessTokenRepository = require('./update-access-token-repository');
+const MongoHelper = require('../helpers/mongo-helper');
+let db;
+
+const makeSut = () => {
+  const userModel = db.collection('users');
+  const sut = new UpdateAccessTokenRepository(userModel);
+  return {
+    sut,
+    userModel
+  };
+};
+
+describe('Update Access Token Repository', () => {
+  let fakeUserId;
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+    db = await MongoHelper.getDb();
+  });
+
+  beforeEach(async () => {
+    const userModel = db.collection('users');
+    await userModel.deleteMany();
+    const fakeUser = await userModel.insertOne({
+      email: 'valid_email@mail.com',
+      name: 'any_name',
+      state: 'any_state',
+      password: 'hashed_password'
+    });
+    fakeUserId = fakeUser.ops[0]._id;
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  test('Should update the user with the given accessToken ', async () => {
+    const { sut, userModel } = makeSut();
+    await sut.update(fakeUserId, 'valid_token');
+    const updatedFakeUser = await userModel.findOne({ _id: fakeUserId });
+    expect(updatedFakeUser.accessToken).toBe('valid_token');
+  });
+
+  test('Should throw if no userModel is provided', async () => {
+    const sut = new UpdateAccessTokenRepository();
+    const promise = sut.update(fakeUserId, 'valid_token');
+    expect(promise).rejects.toThrow();
+  });
+
+  test('Should throw if no params are provided', async () => {
+    const { sut } = makeSut();
+    expect(sut.update()).rejects.toThrow(new MissingParamError('userId'));
+    expect(sut.update(fakeUserId)).rejects.toThrow(new MissingParamError('accessToken'));
+  });
+});

6dfb24c5430c47a3288978c5feab2191fe641942
Elcio Junior <julioneljunior@gmail.com>
2020-07-28 19:48:53 -0300

    fix(src/domain/usecases/auth-usecase): use _id in generate-token

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -151,7 +151,7 @@ describe('Auth usecase', () => {
   test('Should call TokenGenerator with correct userId', async () => {
     const { sut, loadUserByEmailRepositorySpy, tokenGeneratorSpy } = makeSut();
     await sut.auth('valid_email@mail.com', 'valid_password');
-    expect(tokenGeneratorSpy.userId).toBe(loadUserByEmailRepositorySpy.user.id);
+    expect(tokenGeneratorSpy.userId).toBe(loadUserByEmailRepositorySpy.user._id);
   });
 
   test('Should return an accessToken if correct credentials are provided', async () => {
@@ -164,7 +164,7 @@ describe('Auth usecase', () => {
   test('Should call UpdateAccessTokenRepository with correct values', async () => {
     const { sut, updateAccessTokenRepositorySpy, loadUserByEmailRepositorySpy, tokenGeneratorSpy } = makeSut();
     await sut.auth('valid_email@mail.com', 'valid_password');
-    expect(updateAccessTokenRepositorySpy.userId).toBe(loadUserByEmailRepositorySpy.user.id);
+    expect(updateAccessTokenRepositorySpy.userId).toBe(loadUserByEmailRepositorySpy.user._id);
     expect(updateAccessTokenRepositorySpy.accessToken).toBe(tokenGeneratorSpy.accessToken);
   });
 

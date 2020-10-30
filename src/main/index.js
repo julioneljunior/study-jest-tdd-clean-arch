747936201f930897615d1849ffd4229ff5f347d0
Elcio Junior <julioneljunior@gmail.com>
2020-10-30 11:36:54 -0300

    feat(project): add docker compose

--- src/main/index.js
+++ src/main/index.js
@@ -4,6 +4,6 @@ const MongoHelper = require('../infra/helpers/mongo-helper');
 MongoHelper.connect(env.mongoUrl)
   .then(() => {
     const app = require('./config/app');
-    app.listen(5858, () => console.log('Server Running!'));
+    app.listen(3000, () => console.log('Server Running!'));
   })
   .catch(console.error);
